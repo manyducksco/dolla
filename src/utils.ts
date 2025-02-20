a44cd96d0386cc71f0fde9c663d297f5d381844c
Tony McCoy <tony@ratwizard.io>
2025-02-19 23:48:53 -0800

    Build in deep equals

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import { isFunction, isObject, typeOf } from "./typeChecking.js";
-import * as _deepEqual from "fast-deep-equal/es6";
 
 export const noOp = () => {};
 
@@ -73,7 +72,64 @@ export function shallowEqual(a: any, b: any): boolean {
 /**
  * Equality check that passes if two objects have equal values, even if they are not the same object.
  */
-export const deepEqual = _deepEqual;
+// NOTE: This code is https://github.com/epoberezkin/fast-deep-equal licensed under MIT.
+// I imported it because I couldn't get the old school module to play nice with my modern ES code as an external dependency.
+export function deepEqual(a: any, b: any): boolean {
+  if (a === b) return true;
+
+  if (a && b && typeof a == "object" && typeof b == "object") {
+    if (a.constructor !== b.constructor) return false;
+
+    var length, i, keys;
+    if (Array.isArray(a)) {
+      length = a.length;
+      if (length != b.length) return false;
+      for (i = length; i-- !== 0; ) if (!deepEqual(a[i], b[i])) return false;
+      return true;
+    }
+
+    if (a instanceof Map && b instanceof Map) {
+      if (a.size !== b.size) return false;
+      for (i of a.entries()) if (!b.has(i[0])) return false;
+      for (i of a.entries()) if (!deepEqual(i[1], b.get(i[0]))) return false;
+      return true;
+    }
+
+    if (a instanceof Set && b instanceof Set) {
+      if (a.size !== b.size) return false;
+      for (i of a.entries()) if (!b.has(i[0])) return false;
+      return true;
+    }
+
+    if (ArrayBuffer.isView(a) && ArrayBuffer.isView(b)) {
+      length = (a as any).length;
+      if (length != (b as any).length) return false;
+      for (i = length; i-- !== 0; ) if ((a as any)[i] !== (b as any)[i]) return false;
+      return true;
+    }
+
+    if (a.constructor === RegExp) return a.source === b.source && a.flags === b.flags;
+    if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
+    if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
+
+    keys = Object.keys(a);
+    length = keys.length;
+    if (length !== Object.keys(b).length) return false;
+
+    for (i = length; i-- !== 0; ) if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
+
+    for (i = length; i-- !== 0; ) {
+      var key = keys[i];
+
+      if (!deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
+  }
+
+  // true if both NaN, false otherwise
+  return a !== a && b !== b;
+}
 
 /*=============================*\
 ||         Object Utils        ||
